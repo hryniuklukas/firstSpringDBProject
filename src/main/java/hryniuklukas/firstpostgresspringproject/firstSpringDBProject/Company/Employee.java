8b59e56c3fdd70b9d3f525f179c937fdb03435d9
hryniuklukas <hryniuklukas@gmail.com>
2022-07-26 21:44:54 +0200

    Lombok added

--- src/main/java/hryniuklukas/firstpostgresspringproject/firstSpringDBProject/Company/Employee.java
+++ src/main/java/hryniuklukas/firstpostgresspringproject/firstSpringDBProject/Company/Employee.java
@@ -1,5 +1,10 @@
 package hryniuklukas.firstpostgresspringproject.firstSpringDBProject.Company;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Objects;
 
 import javax.persistence.Entity;
@@ -7,12 +12,19 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
 public class Employee {
+    @EqualsAndHashCode.Include
     private @Id @GeneratedValue Long id;
+    @EqualsAndHashCode.Include
     private String name;
+    @EqualsAndHashCode.Include
     private String role;
 
-    public Employee(){};
 
     public Employee(String name, String role)
     {
@@ -20,45 +32,4 @@ public class Employee {
         this.role = role;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-    @Override
-    public String toString() {
-        return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
-    }
-
-    @Override
-    public boolean equals(Object o)
-    {
-        if(this == o){return true;}
-        if(!(o instanceof Employee)){return false;}
-        Employee employee = (Employee) o;
-        return Objects.equals(this.id,employee.id) && Objects.equals(this.name, employee.name) && Objects.equals(this.role, employee.role);
-    }
-    @Override
-    public int hashCode()
-    {
-        return Objects.hash(this.id, this.name, this.role);
-    }
 }
--- src/main/java/hryniuklukas/firstpostgresspringproject/firstSpringDBProject/Configs/LoadDB.java
+++ src/main/java/hryniuklukas/firstpostgresspringproject/firstSpringDBProject/Configs/LoadDB.java
@@ -4,7 +4,8 @@ import hryniuklukas.firstpostgresspringproject.firstSpringDBProject.Company.Empl
 import hryniuklukas.firstpostgresspringproject.firstSpringDBProject.Repos.EmployeeRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationRunner;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,10 +15,10 @@ public class LoadDB {
     private static final Logger log = LoggerFactory.getLogger(LoadDB.class);
 
     @Bean
-    CommandLineRunner initDatabase(EmployeeRepo employeeRepo) {
+    ApplicationRunner initDatabase(EmployeeRepo employeeRepo) {
         return args -> {
-            log.info("Preloading " + employeeRepo.save(new Employee("Tom Kowalski", "worker")));
-            log.info("Preloading " + employeeRepo.save(new Employee("John Novak", "forklift driver")));
+            log.info("Preloading " + employeeRepo.save(new Employee("Tom Novak", "worker")));
+            log.info("Preloading " + employeeRepo.save(new Employee("John Kowalski", "forklift driver")));
         };
     }
 
